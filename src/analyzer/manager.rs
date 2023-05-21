ea0407570bec9525f5d926b2dcfe6265440c2da8
Mimea005 <30933848+Mimea005@users.noreply.github.com>
2023-05-21 11:03:20 +0200

    Moved Text loading to the main thread

--- src/analyzer/manager.rs
+++ src/analyzer/manager.rs
@@ -1,71 +1,168 @@
 use std::{
-    fs::File,
-    io::{self, Seek, Write},
+    eprintln,
+    io::{self, ErrorKind, Read, Seek},
+    println,
     sync::{Arc, Mutex},
     thread,
 };
 
-use super::{AnalyzerResult, AnalyzerThread};
+use super::AnalyzerResult;
 
-pub struct AnalyzerManager {
+pub struct AnalyzerManager<S: Read + Seek> {
     thread_count: usize,
     thread_block_size: usize,
-    text_stream: Arc<Mutex<File>>,
+    text_stream: S,
+    text_stream_length: Option<u64>,
 }
 
-impl AnalyzerManager {
-    pub fn new(thread_count: usize, thread_block_size: usize, text_stream: File) -> Self {
-        println!(
-            "AnalyzerManager: file has {} bytes",
-            text_stream.metadata().unwrap().len()
-        );
-        Self {
+impl<S: Read + Seek> AnalyzerManager<S> {
+    pub fn new(thread_count: usize, thread_block_size: usize, text_stream: S) -> Self {
+        let mut manager = Self {
             thread_count,
             thread_block_size,
-            text_stream: Arc::new(Mutex::new(text_stream)),
+            text_stream,
+            text_stream_length: None,
+        };
+
+        if let Ok(bytes) = manager.text_stream.seek(io::SeekFrom::End(0)) {
+            println!("AnalyzerManager: file has {bytes} bytes");
+            manager
+                .text_stream
+                .seek(io::SeekFrom::Start(0))
+                .expect("Failed to set curso to start");
+            manager.text_stream_length = Some(bytes);
+        } else {
+            println!("AnalyzerManager: stream has no defined end");
         }
+
+        manager
     }
 
-    pub fn analyze(self) -> AnalyzerResult {
+    pub fn analyze(mut self) -> AnalyzerResult {
         let mut result = AnalyzerResult::new();
 
+        // 1. Create txt buffer channel
+        let (snd_buffer, rcv_buffer) = std::sync::mpsc::channel::<String>();
+        let rcv_buffer = Arc::new(Mutex::new(rcv_buffer));
+        // 2. Create worker threads
         thread::scope(|s| {
-            let mut threads = Vec::new();
+            let mut threads = Vec::with_capacity(self.thread_count);
 
-            for _ in 0..self.thread_count {
-                let thr = AnalyzerThread::with_block_size(
-                    self.thread_block_size,
-                    self.text_stream.clone(),
-                );
-                threads.push(s.spawn(|| thr.analyze()));
-            }
+            for id in 0..self.thread_count {
+                let rcv_buffer = rcv_buffer.clone();
 
-            let (tx_stop, rx_stop) = std::sync::mpsc::channel(); // Create a channel to send a stop signal
-            let thr_args = (self.text_stream.clone(), rx_stop);
-            s.spawn(move || {
-                while thr_args.1.try_recv().is_err() {
-                    if let Ok(mut stream) = thr_args.0.lock() {
-                        let curr = stream.stream_position().unwrap();
-                        let len = stream.metadata().unwrap().len();
-                        drop(stream);
+                threads.push(s.spawn(move || {
+                    let id = id;
+                    let mut result = AnalyzerResult::new();
+                    loop {
+                        let txt_query = rcv_buffer.lock().unwrap().recv();
 
-                        let progress = (curr as f64 / len as f64) * 100.0;
-                        print!("\rProgress: {:.2}%\t", progress);
-                        io::stdout().flush().unwrap();
+                        if let Ok(buffer) = txt_query {
+                            eprintln!("Worker {id} received buffer");
+                            let buffer = buffer.split_whitespace().map(|s| s.to_string());
+                            result.analyze(buffer);
+                        } else {
+                            break;
+                        }
                     }
 
-                    thread::sleep(std::time::Duration::from_millis(100));
-                }
-                print!("\r                             \r"); // Clear the progress line
-            });
+                    result
+                }));
+            }
+            // 3. Load text buffer and send through channel
 
-            for join in threads {
-                result += join.join().unwrap();
+            let mut total_read = 0;
+            loop {
+                let mut buffer = vec![0; self.thread_block_size];
+
+                match self.text_stream.read(&mut buffer) {
+                    Ok(bytes) => {
+                        if let Ok(mut txt) = String::from_utf8(buffer) {
+                            total_read += bytes;
+
+                            // A word that ends without a space may not be complete, therefore we
+                            // continue to read until a whitespace is found or the stream is empty
+                            if !txt.ends_with(char::is_whitespace) && bytes == total_read {
+                                let mut bytes: Vec<u8> = Vec::new();
+                                let mut byte = [0];
+                                loop {
+                                    match self.text_stream.read(&mut byte) {
+                                        Ok(b) => {
+                                            if b == 0 {
+                                                break;
+                                            }
+                                            bytes.extend_from_slice(&byte);
+                                            total_read += 1;
+
+                                            // If this fails to interpret as utf8 then the character is
+                                            // not complete yet
+                                            if let Ok(word) = std::str::from_utf8(bytes.as_slice())
+                                            {
+                                                if word.ends_with(char::is_whitespace) {
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        Err(e) => {
+                                            // Reached end of stream
+                                            if e.kind() == std::io::ErrorKind::WouldBlock
+                                                || e.kind() == std::io::ErrorKind::UnexpectedEof
+                                            {
+                                                break;
+                                            } else {
+                                                // An actual error
+                                                panic!("Error reading from stream: {}", e);
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if let Ok(word) = std::str::from_utf8(bytes.as_slice()) {
+                                    txt.push_str(word);
+                                }
+                            }
+
+                            snd_buffer.send(txt).unwrap();
+                            eprintln!("Sendt {bytes} long buffer to threads");
+
+                            // This is the end of the stream, last
+                            // word will never complete if not
+                            // already
+                            if bytes != self.thread_block_size {
+                                println!("Finished loading stream");
+                                break;
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        if e.kind() == ErrorKind::Interrupted {
+                            continue;
+                        } else {
+                            eprintln!("Encountered while reading file {e}");
+                        }
+                    }
+                }
+
+                if let Some(length) = self.text_stream_length {
+                    print!(
+                        "\rAnalyzed {:.2}% of file\t",
+                        (total_read as f64 / length as f64) * 100.0
+                    );
+                } else {
+                    println!("Analyzed {} bytes", total_read);
+                }
             }
 
-            tx_stop.send(true).unwrap();
-        });
+            // 4. when file is empty, close txt buffer channel
+            // 5. join worker threads and combine results
+            // 6. return result
 
+            drop(snd_buffer);
+
+            for worker in threads {
+                result += worker.join().unwrap();
+            }
+        });
         result
     }
 }
